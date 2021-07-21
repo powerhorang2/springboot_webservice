0f70a352d8c3f2946e1c8d367a7ce6f9533550b3
powerhorang2 <tjrdbqls3@naver.com>
2021-07-21 17:43:32 +0900

    Postman + 웹 콘솔로 검증
    2021-07-21 / powerhorang2

--- src/main/java/com/jojoldu/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/domain/posts/PostsRepository.java
@@ -1,4 +1,4 @@
-package com.jojoldu.posts;
+package com.jojoldu.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
--- src/main/java/com/jojoldu/web/WebRestController.java
+++ src/main/java/com/jojoldu/web/WebRestController.java
@@ -1,15 +1,30 @@
 package com.jojoldu.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jojoldu.domain.posts.PostsRepository;
+import com.jojoldu.dto.posts.PostsSaveRequestDto;
+
+import lombok.AllArgsConstructor;
 
 //@RestController 는 @ResponseBody 를 모든 메소드에 적용 해준다.
 //hello 메소드의 결과는 HelloWorld 라는 문자열을 JSON 형태로 반환한다.
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+	
+	private PostsRepository postsRepository;
 
 	@GetMapping("/hello")
 	public String hello() {
 		return "HelloWorld";
+	}
+	
+	@PostMapping("/posts")
+	public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+		postsRepository.save(dto.toEntity());
 	}
 }
