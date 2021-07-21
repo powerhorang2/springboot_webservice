d5aa72a35cce097a4fe4ba1fbc540a60a083aae6
powerhorang2 <tjrdbqls3@naver.com>
2021-07-21 16:21:32 +0900

    properties -> yml 으로 수정 Entity와 Repository 생성 후 Test
    2021-07-21 / powerhorang2

---
+++ src/main/java/com/jojoldu/posts/Posts.java
@@ -0,0 +1,41 @@
+package com.jojoldu.posts;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/*
+ * Posts 클래스는 실제 DB의 테이블과 매칭될 클래스이고, Entity 클래스임
+ * JPA를 사용하면 DB데이터에 작업할 경우 실제 쿼리를 날리기 보다는, 이 Entity(Posts) 클래스 수정을 통해 작업함
+ */
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Posts {
+
+	@Id
+	@GeneratedValue // 기본 값이 AUTO
+	private Long id;
+	
+	@Column(length = 500, nullable = false)
+	private String title;
+	
+	@Column(columnDefinition = "TEXT", nullable = false)
+	private String content;
+	
+	private String author;
+	
+	@Builder
+	public Posts(String title, String content, String author) {
+		this.title = title;
+		this.content = content;
+		this.author = author;
+	}
+}
